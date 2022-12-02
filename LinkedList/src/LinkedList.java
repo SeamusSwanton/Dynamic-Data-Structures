4981d8b6d553c68cabdb3ed8ce260e18d11e0e59
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-12-02 12:04:31 -0700

    initial commit

---
+++ LinkedList/src/LinkedList.java
@@ -0,0 +1,39 @@
+
+public class LinkedList {
+
+	public Object size() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object get(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void remove(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void add(int i, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void set(int i, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void add(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
