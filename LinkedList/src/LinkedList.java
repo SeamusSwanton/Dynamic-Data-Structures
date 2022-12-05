10594ff7c65617dd9b996ebafd75418a072a9331
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-12-05 13:37:41 -0700

    cswhwe

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,35 +1,48 @@
 
 public class LinkedList {
 
-	public Object size() {
+public Node head = null;
+public Node tail = null;
+	
+	
+	public void add(String element) {
+		Node newNode = new Node(element,null,null);
+		if (head == null) {
+			 
+			 head = tail = newNode;
+		}
+		else {
+			tail.next = newNode;
+			newNode.previous = tail;
+			
+		}
+		
+	}
+
+	public void add(int index, String element) {
+//		Node newNode = new Node(element, newNode, newNode);
+		
+	}
+
+	public int size() {
 		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String get(int index) {
+		
 		return null;
 	}
 
-	public Object get(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void remove(int i) {
+	public void remove(int index) {
 		// TODO Auto-generated method stub
 		
 	}
-
-	public void add(int i, String string) {
+		
+	public void set(int index, String element) {
 		// TODO Auto-generated method stub
 		
-	}
-
-	public void set(int i, String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void add(String string) {
-		// TODO Auto-generated method stub
-		
-	}
+	}	
 
 	public void clear() {
 		// TODO Auto-generated method stub
