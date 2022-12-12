4c4fe2aea86238a86550c23c3b7b6e81221d9a30
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-12-12 13:28:56 -0700

    e

--- Linked list(1)/src/LinkedList.java
+++ Linked list(1)/src/LinkedList.java
@@ -1,6 +1,6 @@
 import java.util.ListIterator;
 
-public class LinkedList {
+public class LinkedList implements ListIterator{
 	public Node head = null;
 	public Node tail = null;
 
@@ -192,5 +192,59 @@ public class LinkedList {
 	public ListIterator listIterator() {
 
 		return null;
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object previous() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int nextIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int previousIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void set(Object e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(Object e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
